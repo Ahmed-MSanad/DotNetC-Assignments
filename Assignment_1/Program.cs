62a451959653cd1e67998c122776f7bedd167ffc
Ahmed <sanada927@gmail.com>
2025-01-26 19:31:41 +0200

    C#_Session_11(OOP_6)_Assignment_9_MySolution

--- Assignment_1/Program.cs
+++ Assignment_1/Program.cs
@@ -1,10 +1,125 @@
-﻿namespace Assignment_1
+﻿using System.Diagnostics;
+using Assignment_1.Project_one.Q1;
+
+namespace Assignment_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Proj 1
+            // 1) Define Class Duration To include Three Attributes Hours, Minutes and Seconds: -> Done
+
+            // 2) Override All System.Object Members (ToString, Equals,GetHasCode): -> Done
+
+            // 3) Define All Required Constructors to Produce this output:
+
+            Duration D11 = new Duration(1, 10, 15);
+            Console.WriteLine($"D11 => {D11.ToString()}");
+
+            Duration D1 = new Duration(3600);
+            Console.WriteLine($"D1 => {D1.ToString()}");
+
+            Duration D2 = new Duration(7800);
+            Console.WriteLine($"D2 => {D2.ToString()}");
+
+            Duration D3 = new Duration(666);
+            Console.WriteLine($"D3 => {D3.ToString()}");
+
+            // 4) Implement All required Operators overloading to enable this Code:
+
+            D3 = D1 + D2;
+            Console.WriteLine($"D3 => {D3.ToString()}");
+            D3 = D1 + null;
+            Console.WriteLine($"D3 => {D3.ToString()}");
+            D3 = null + D2;
+            Console.WriteLine($"D3 => {D3.ToString()}");
+
+            D3 = D1 + 7800;
+            Console.WriteLine($"D3 => {D3.ToString()}");
+
+            D3 = 666 + D3;
+            Console.WriteLine($"D3 => {D3.ToString()}");
+
+            D3 = ++D1; // (Increase One Minute)
+            Console.WriteLine($"D3 => {D3.ToString()} and new D1 is D1: {D1.ToString()}");
+
+            D3 = --D2; // (Decrease One Minute)
+            Console.WriteLine($"D3 => {D3.ToString()} and new D2 is D2: {D2.ToString()}");
+
+            D1 = D1 - D2;
+            Console.WriteLine($"D1 => {D1.ToString()}");
+
+            if(D1 > D2){
+                Console.WriteLine($"D1: {D1.ToString()} is greater than D2: {D2.ToString()}");
+            }
+            else
+            {
+                Console.WriteLine($"D1: {D1.ToString()} is not greater than D2: {D2.ToString()}");
+            }
+
+            if(D1 <= D2){
+                Console.WriteLine($"D1: {D1.ToString()} is less than or equal D2: {D2.ToString()}");
+            }
+            else
+            {
+                Console.WriteLine($"D1: {D1.ToString()} is not less than or equal D2: {D2.ToString()}");
+            }
+
+            if(D1){
+                Console.WriteLine($"D1 => {D1.ToString()} -> is Not Zero Duration.");
+            }
+            else
+            {
+                Console.WriteLine($"D1 => {D1.ToString()} -> is Zero Duration.");
+            }
+
+            DateTime Obj = (DateTime)D1;
+            Console.WriteLine(Obj);
+
+
+            // Cases Should be handled:
+            Console.WriteLine($"==================================== Extra Cases I handled them: ====================================");
+            D1 = new Duration(1, 0, 0);
+            Console.WriteLine($"D1 => {D1.ToString()}");
+            --D1;
+            Console.WriteLine($"D1 => {D1.ToString()}");
+
+
+            Console.WriteLine("================== ===================");
+            D1 = new Duration(0, 0, 4);
+            Console.WriteLine($"D1 => {D1.ToString()}");
+            try { 
+                --D1;
+                Console.WriteLine($"D1 => {D1.ToString()}");
+            } catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+            Console.WriteLine("================== ===================");
+            D1 = new Duration(0, 59, 0);
+            Console.WriteLine($"D1 => {D1.ToString()}");
+            ++D1;
+            Console.WriteLine($"D1 => {D1.ToString()}");
+
+
+            Console.WriteLine("================== ===================");
+            D1 = null;
+            try
+            {
+                Obj = (DateTime)D1;
+                Console.WriteLine(Obj);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+
+            #endregion
         }
     }
 }
---
+++ Assignment_1/Project_one/Q1/Duration.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1.Project_one.Q1
+{
+    public class Duration
+    {
+        #region 1) Define Class Duration To include Three Attributes Hours, Minutes and Seconds:
+
+        public int hours;
+        public int minutes;
+        public int seconds;
+
+        #endregion
+
+        #region 2) Override All System.Object Members (ToString, Equals,GetHasCode) 
+        public override string ToString()
+        {
+            if (hours != 0)
+                return $"Hours: {hours},  Minutes: {minutes}, Seconds: {seconds}";
+            else if (minutes != 0)
+                return $"Minutes: {minutes}, Seconds: {seconds}";
+            else if (seconds != 0)
+                return $"Seconds: {seconds}";
+            else
+                return $"00 : 00 : 00";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            Duration other = (Duration)obj;
+            return hours == other.hours && minutes == other.minutes && seconds == other.seconds;
+        }
+
+        public override int GetHashCode()
+        {
+            //int hashCode = hours.GetHashCode();
+            //hashCode += minutes.GetHashCode();
+            //hashCode += seconds.GetHashCode();
+            //return hashCode;
+
+            // OR:
+            return HashCode.Combine(hours, minutes, seconds);
+        }
+        #endregion
+
+
+        #region 3) Define All Required Constructors to Produce this output:
+        public Duration() { }
+
+        public Duration(int _hours, int _minutes, int _seconds) {
+            hours = _hours;
+            minutes = _minutes;
+            seconds = _seconds;
+        }
+
+        public Duration(int timeInSeconds) {
+            hours = timeInSeconds / 3600;
+            minutes = (timeInSeconds % 3600) / 60;
+            seconds = timeInSeconds % 60;
+        }
+
+        #endregion
+
+        #region 4) Implement All required Operators overloading to enable this Code:
+
+        public static Duration operator +(Duration a, Duration b) {
+            return new Duration
+            {
+                hours = (a?.hours ?? 0) + (b?.hours ?? 0),
+                minutes = (a?.minutes ?? 0) + (b?.minutes ?? 0),
+                seconds = (a?.seconds ?? 0) + (b?.seconds ?? 0),
+            };
+        }
+
+        public static Duration operator +(Duration a, int b)
+        {
+            return new Duration
+            {
+                hours = (a?.hours ?? 0) + (b / 3600),
+                minutes = (a?.minutes ?? 0) + ((b % 3600) / 60),
+                seconds = (a?.seconds ?? 0) + (b % 60),
+            };
+        }
+
+        public static Duration operator +(int b, Duration a)
+        {
+            return new Duration
+            {
+                hours = (a?.hours ?? 0) + (b / 3600),
+                minutes = (a?.minutes ?? 0) + ((b % 3600) / 60),
+                seconds = (a?.seconds ?? 0) + (b % 60),
+            };
+        }
+
+        public static Duration operator ++(Duration d)
+        {
+            if (d == null)
+            {
+                throw new Exception("The Duration is null.");
+            }
+            else if (d.minutes != 59)
+            {
+                d.minutes++;
+            }
+            else if (d.minutes == 59)
+            {
+                d.hours++;
+                d.minutes = 0;
+            }
+            return d;
+        }
+
+        public static Duration operator --(Duration d)
+        {
+            if (d == null || (d.minutes == 0 && d.hours == 0))
+            {
+                throw new Exception("The Duration is either null or there is no enough hours and minutes to decrease.");
+            }
+            else if (d.minutes != 0)
+            {
+                d.minutes--;
+            }
+            else if (d.hours != 0) {
+                d.hours--;
+                d.minutes = 59;
+            }
+            return d;
+        }
+
+        public static Duration operator -(Duration a, Duration b)
+        {
+            return new Duration
+            {
+                hours = Math.Abs((a?.hours ?? 0) - (b?.hours ?? 0)),
+                minutes = Math.Abs((a?.minutes ?? 0) - (b?.minutes ?? 0)),
+                seconds = Math.Abs((a?.seconds ?? 0) - (b?.seconds ?? 0)),
+            };
+        }
+
+        public static bool operator >(Duration Left, Duration Right)
+        {
+            return (Left?.hours ?? 0) > (Right?.hours ?? 0) ||
+                    (Left?.minutes ?? 0) > (Right?.minutes ?? 0) ||
+                    (Left?.seconds ?? 0) > (Right?.seconds ?? 0);
+        }
+
+        public static bool operator <(Duration Left, Duration Right)
+        {
+            return (Left?.hours ?? 0) < (Right?.hours ?? 0) ||
+                    (Left?.minutes ?? 0) < (Right?.minutes ?? 0) ||
+                    (Left?.seconds ?? 0) < (Right?.seconds ?? 0);
+        }
+
+        public static bool operator >=(Duration Left, Duration Right)
+        {
+            return (Left?.hours ?? 0) >= (Right?.hours ?? 0) ||
+                    (Left?.minutes ?? 0) >= (Right?.minutes ?? 0) ||
+                    (Left?.seconds ?? 0) >= (Right?.seconds ?? 0);
+        }
+
+        public static bool operator <=(Duration Left, Duration Right)
+        {
+            return (Left?.hours ?? 0) <= (Right?.hours ?? 0) ||
+                    (Left?.minutes ?? 0) <= (Right?.minutes ?? 0) ||
+                    (Left?.seconds ?? 0) <= (Right?.seconds ?? 0);
+        }
+
+        public static bool operator true(Duration d) {
+            return (d.hours > 0) || (d.minutes > 0) || (d.seconds > 0);
+        }
+
+        public static bool operator false(Duration d)
+        {
+            return (d.hours == 0) && (d.minutes == 0) && (d.seconds == 0);
+        }
+
+        public static explicit operator DateTime(Duration d)
+        {
+            try
+            {
+                DateTime retDateTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, d.hours, d.minutes, d.seconds);
+                return retDateTime;
+            }
+            catch (Exception e) {
+                throw new Exception("Can't Convert to DateTime as the Duration Provided is NULL!!");
+            }
+        }
+
+        #endregion
+
+    }
+}
